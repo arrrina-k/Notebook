385fed4686676b7415bf8182f59e69c386daa42a
arrrina-k <krivenko.arinka@yandex.ru>
2020-08-21 15:37:07 +0300

    Initial Commit

---
+++ Notebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notebook
+//
+//  Created by Арина Кривенко on 21.08.2020.
+//  Copyright © 2020 Арина Кривенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
